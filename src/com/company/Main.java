2b0f7cba431e20e11e7117fa5efaf3ac9fb29e89
SimZor <arledal.simon@gmail.com>
2017-09-02 00:09:17 +0200

    Added exchange rates API - Pull data from it.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import com.google.gson.Gson;
 
 class CurrencySettings {
     private double currencyValue;
@@ -25,9 +31,56 @@ class CurrencySettings {
     }
 }
 
+class Rates {
+    public Double SEK;
+}
+
+class CurrencyData {
+    public String base;
+    public String date;
+    public Object rates;
+}
+
 public class Main {
 
-    public static void startShoppingHelper(Scanner inputReader, CurrencySettings currency) {
+    public static double getExchangeRate(String currency) throws IOException {
+        URL url = null;
+        HttpURLConnection connection = null;
+        InputStreamReader inputStreamReader = null;
+
+        url = new URL("https://api.fixer.io/latest?symbols=SEK&base=" + currency);
+
+        connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setReadTimeout(5000);
+        connection.setConnectTimeout(5000);
+        connection.setInstanceFollowRedirects(false);
+
+        inputStreamReader = new InputStreamReader(connection.getInputStream());
+        BufferedReader in = new BufferedReader(inputStreamReader);
+
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+
+        in.close();
+        connection.disconnect();
+
+        Gson g = new Gson();
+        CurrencyData currencyData = g.fromJson(String.valueOf(content), CurrencyData.class);
+        Rates currencyRates = g.fromJson(String.valueOf(currencyData.rates), Rates.class);
+
+        System.out.println(content.toString());
+
+        double currencyRate = currencyRates.SEK;
+        return currencyRate;
+    }
+
+    public static void startShoppingHelper(Scanner inputReader, CurrencySettings currency) throws IOException {
         System.out.println("\n----------------------------------- Shopping Helper Functions -----------------------------------");
 
         if (currency.getCurrencyIdentifier() != null || currency.getCurrencyValue() != 0) {
@@ -105,11 +158,15 @@ public class Main {
                 System.out.print("\nCurrency Identifier (Examples: USD, AUD etc): ");
                 String currencyID = inputReader.next();
 
-                System.out.print("Currency value of " + currencyID + ": ");
+                System.out.print("Currency value of " + currencyID + " (Input negative value to pull data from online exchange): ");
                 double currencyValue = inputReader.nextDouble();
 
-                currency.setCurrencyValue(currencyValue);
+                if (currencyValue < 0) {
+                    currencyValue = getExchangeRate(currencyID);
+                }
+
                 currency.setCurrencyIdentifier(currencyID);
+                currency.setCurrencyValue(currencyValue);
 
                 System.out.println("Currency information has been updated.");
         }
@@ -118,7 +175,7 @@ public class Main {
         startShoppingHelper(inputReader, currency);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         final double version = 0.1;
         System.out.println("\n\nWelcome to Shopping Helper v" + version + "!");
         System.out.println("Created by Simon Arledal at 2017.08.31");
